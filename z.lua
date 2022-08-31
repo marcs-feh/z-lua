af8907a61dfbe41e15b9d250d8dccb8c400e618f
marcs-feh <marcosfehlauer@protonmail.com>
2022-08-31 20:41:05 -0300

    add archive struct

--- z.lua
+++ z.lua
@@ -1,5 +1,47 @@
 #!/usr/bin/env lua
 
+---Metatable for archives
+local ar_mt = {
+	__tostring = function (self)
+		local s = '['.. self.name .. ' : ' .. self.comp_algo ..']\n'
+		local old_s = s
+			for f, _ in pairs(self.entries) do
+				s = s .. '\t\'' .. f .. '\'\n'
+			end
+		if s == old_s then
+			s = s .. '\t(empty)\n'
+		end
+		return s
+	end,
+	__metatable = false
+}
+
+---Creates an Archive.
+---@param t table
+---@return table
+local function Archive(t)
+	local ar = {
+		comp_algo = t.comp_algo or 'gzip',
+		entries   = t.entries or {},
+		name      = t.name or 'untitled-archive',
+		addEntry  = function (self, file)
+			self.entries[file] = true
+		end,
+		delEntry  = function (self, file)
+			self.entries[file] = nil
+		end
+	}
+
+	t.entries = t.entries or {}
+	for _, v in ipairs(t.entries) do
+		ar.entries[v] = true
+	end
+
+	setmetatable(ar, ar_mt)
+	return ar
+end
+
+
 ---Compression algos and their extensions.
 local COMP_ALGORITHMS = {
 	['xz'] = 'xz',
@@ -8,19 +50,26 @@ local COMP_ALGORITHMS = {
 	['bzip'] = 'bz'
 }
 
+---Global settings
 local settings = {
 	algo  = 'gzip',
 	force = false,
 }
 
+---Archive list
+local archives = {}
+
+---Flags and their actions
 local flags = {
 	['-c'] = function(arg)
 		if not COMP_ALGORITHMS[arg] then
-			error('Invalid Compression algorithm: ' .. tostring(arg), 2)
+			error('Invalid compression algorithm: ' .. tostring(arg), 2)
 		end
 	end,
 	['-f'] = function()
 		settings.force = true
-	end
+	end,
 }
+
+--- Main
 
