0bca2733c215f4cd1ef857ca0ae7fcc51d817fc0
marcs-feh <marcosfehlauer@protonmail.com>
2022-09-01 11:32:37 -0300

    add more flags, start work on module support

--- z.lua
+++ z.lua
@@ -53,6 +53,8 @@ local COMP_ALGORITHMS = {
 ---Global settings
 local settings = {
 	algo  = 'gzip',
+	bundle = false,
+	override_name = nil, -- Set this to a string and it becomes the forced name
 	force = false,
 }
 
@@ -60,16 +62,69 @@ local settings = {
 local archives = {}
 
 ---Flags and their actions
-local flags = {
+local flags = {}
+flags = {
 	['-c'] = function(arg)
 		if not COMP_ALGORITHMS[arg] then
-			error('Invalid compression algorithm: ' .. tostring(arg), 2)
+			error('Invalid compression algorithm: ' .. tostring(arg))
 		end
 	end,
+
 	['-f'] = function()
 		settings.force = true
 	end,
+
+	['-b'] = function()
+		settings.bundle = true
+	end,
+
+	['-o'] = function(name)
+		if not name or flags[name] then
+			error('No provided output for -o')
+		end
+		settings.bundle = true
+		settings.override_name = tostring(name)
+	end,
+
+	['-h'] = function()
+		print('usage: z [-l MOD] [-c ALGO] [-o NAME] [-fh] TARGETS\n'
+			  ..'\t-f       Override existing archives\n'
+			  ..'\t-c ALGO  Use ALGO as compression algorithm\n'
+			  ..'\t-b       Bundle all targets into a single archive\n'
+			  ..'\t-o NAME  Use NAME as archive output, assumes -b\n'
+				..'\t-l MOD   Load lua module MOD and use its Archives field to run the\n'
+				..'\t         program, this will cause the program to ignore any targets\n'
+				..'\t         provided through the command line, supresses: -b,-o,-c\n'
+			  ..'\t-h       Display this help message\n'
+			  ..'\t--       Stop parsing options after -\n'
+		)
+		os.exit(0)
+	end,
+
+	['-l'] = function(mod)
+		if not mod then
+			error('No module provided')
+		end
+		local s = require(mod)
+		if not s.Archives then
+			error('No archive schema found in Lua module: ' .. mod)
+		end
+	end,
+
+	['--'] = function()
+		flags = {}
+	end
 }
 
 --- Main
+
+local targets = {}
+for i, a in ipairs(arg) do
+	if flags[a] then
+		flags[a](arg[i+1])
+	else
+		targets[#targets+1] = a
+		print('Target :', a)
+	end
+end
 
