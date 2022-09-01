88e0463c2ceee42793597f866ff0b054bd2b52d4
marcs-feh <marcosfehlauer@protonmail.com>
2022-09-01 16:44:25 -0300

    verbosity and remove useless option

--- z.lua
+++ z.lua
@@ -1,9 +1,37 @@
 #!/usr/bin/env lua
 
+---Global settings
+local settings = {
+	algo  = 'gzip',
+	bundle = false,
+	override_name = nil, -- Set this to a string and it becomes the forced name
+	force = false,
+	use_module = false,
+	verbose = false
+}
+
+---Log function for verbose mode
+local function log(msg)
+	if settings.verbose then
+		io.stderr:write(tostring(msg) .. '\n')
+	end
+end
+
+--- Utility function to expand table to string
+local function expandKeys(tbl)
+	local s = ''
+	for k, _ in pairs(tbl) do
+		if type(k) == 'string' then
+			s = s .. "'" .. tostring(k) .. "' "
+		end
+	end
+	return s
+end
+
 ---Metatable for archives
 local ar_mt = {
 	__tostring = function (self)
-		local s = '[\27[0;34m'.. self.name .. '\27[0m | \27[0;33m' .. self.comp_algo ..'\27[0m]\n'
+		local s = '[\27[0;34m'.. self.name .. '\27[0m : \27[0;33m' .. self.comp_algo ..'\27[0m]\n'
 		local old_s = s
 			for f, _ in pairs(self.entries) do
 				s = s .. "  \27[0;36m*\27[0m '" .. f .. "'\n"
@@ -42,17 +70,6 @@ local function Archive(t)
 	return ar
 end
 
---- Utility function to expand table to string
-local function expandKeys(tbl)
-	local s = ''
-	for k, _ in pairs(tbl) do
-		if type(k) == 'string' then
-			s = s .. "'" .. tostring(k) .. "' "
-		end
-	end
-	return s
-end
-
 ---Command to use for tape archiver
 local TAR_CMD = 'tar'
 
@@ -60,27 +77,27 @@ local TAR_CMD = 'tar'
 local COMP_ALGORITHMS = {
 	['xz'] = function(out, files)
 		local cmd = TAR_CMD .. " cJf '".. out ..".tar.xz' " .. expandKeys(files)
-		print(cmd)
+		log('Compressing with: ' .. cmd)
 		os.execute(cmd)
 	end,
 	['gzip'] = function(out, files)
 		local cmd = TAR_CMD .. " czf '".. out ..".tar.gz' " .. expandKeys(files)
-		print(cmd)
+		log('Compressing with: ' .. cmd)
 		os.execute(cmd)
 	end,
 	['bzip'] = function(out, files)
 		local cmd = TAR_CMD .. " cjf '".. out ..".tar.bz' " .. expandKeys(files)
-		print(cmd)
+		log('Compressing with: ' .. cmd)
 		os.execute(cmd)
 	end,
 	['zstd'] = function(out, files)
 		local cmd = TAR_CMD .. ' cf - '.. expandKeys(files) .. ' | zstd -o ' .. "'".. out ..".tar.zst'"
-		print(cmd)
+		log('Compressing with: ' .. cmd)
 		os.execute(cmd)
 	end,
 	['lz4'] = function(out, files)
 		local cmd = TAR_CMD .. ' cf - '.. expandKeys(files) .. ' | lz4 - ' .. "'".. out ..".tar.lz4'"
-		print(cmd)
+		log('Compressing with: ' .. cmd)
 		os.execute(cmd)
 	end,
 }
@@ -88,23 +105,14 @@ local COMP_ALGORITHMS = {
 --- Help message
 local HELP = ('usage: z [-l MOD] [-c ALGO] [-o NAME] [-fh] TARGETS\n'
             ..'\t-f       Override existing archives\n'
+            ..'\t-v       Be verbose\n'
             ..'\t-c ALGO  Use ALGO as compression algorithm\n'
-            ..'\t-b       Bundle all targets into a single archive\n'
-            ..'\t-o NAME  Use NAME as archive output, assumes -b\n'
+            ..'\t-o NAME  Use NAME as archive output\n'
             ..'\t-l MOD   Load lua module MOD and use its Archives field to run the\n'
             ..'\t         program, this will cause the program to ignore any targets\n'
-            ..'\t         provided through the command line, supresses: -b,-o,-c\n'
+            ..'\t         provided through the command line, supresses: -o,-c\n'
             ..'\t-h       Display this help message\n'
             ..'\t--       Stop parsing options after -\n')
-
----Global settings
-local settings = {
-	algo  = 'gzip',
-	bundle = false,
-	override_name = nil, -- Set this to a string and it becomes the forced name
-	force = false,
-	use_module = false
-}
 
 ---Archive list
 local archives = {}
@@ -130,8 +138,8 @@ flags = {
 		return 0
 	end,
 
-	['-b'] = function()
-		settings.bundle = true
+	['-v'] = function()
+		settings.verbose = true
 		return 0
 	end,
 
@@ -155,6 +163,7 @@ flags = {
 			error('No module provided')
 		end
 		mod = mod:gsub('%.lua$', '') -- remove .lua extension for convenience
+		log('Loading module: ' .. mod)
 		local s = require(mod)
 		if not s or type(Archives) ~= 'table' then
 			error('No archive schema found in Lua module: ' .. mod)
@@ -192,17 +201,23 @@ while i <= #arg do
 	i = i + 1
 end
 
+log('Current settings')
+for k, v in pairs(settings) do
+	log(' ' .. k .. ': ' .. tostring(v))
+end
+
 if settings.use_module then
 	for _, ar in ipairs(archives) do
 		print(ar)
 		COMP_ALGORITHMS[ar.comp_algo](ar.name, ar.entries)
 	end
 elseif #targets > 0 then
-	local out = Archive{name = settings.override_name or targets[1], comp_algo = settings.algo}
+	local ar = Archive{name = settings.override_name or targets[1], comp_algo = settings.algo}
 	for _, v in ipairs(targets) do
-		out:addEntry(v)
+		ar:addEntry(v)
 	end
-	COMP_ALGORITHMS[out.comp_algo](out.name, out.entries)
+	print(ar)
+	COMP_ALGORITHMS[ar.comp_algo](ar.name, ar.entries)
 else
 	--print help and exit
 	print(HELP)
