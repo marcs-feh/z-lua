9d59dc86809c254b07fb23cba2a3885bf5fb3e77
marcs-feh <marcosfehlauer@protonmail.com>
2023-09-04 18:30:10 -0300

    print

--- install.sh
+++ install.sh
@@ -3,25 +3,32 @@
 install_dir="$HOME/.local/bin"
 lua_shebang="#!/usr/bin/env luajit"
 
+Run(){ echo "$@"; "$@"; }
+
 Install (){
-	mkdir -p $install_dir
-	cp z.lua "$install_dir/z"
-	cp uz.lua "$install_dir/uz"
-	chmod 0755 "$install_dir/z"
-	chmod 0755 "$install_dir/uz"
-	sed -e "s,---SHEBANG---,$lua_shebang," "$install_dir/z" -i
-	sed -e "s,---SHEBANG---,$lua_shebang," "$install_dir/uz" -i
+	Run mkdir -p "$install_dir"
+	Run cp z.lua "$install_dir/z"
+	Run cp uz.lua "$install_dir/uz"
+	Run chmod 0755 "$install_dir/z"
+	Run chmod 0755 "$install_dir/uz"
+	Run sed -e "s,---SHEBANG---,$lua_shebang," "$install_dir/z" -i
+	Run sed -e "s,---SHEBANG---,$lua_shebang," "$install_dir/uz" -i
 	exit
 }
 
 Uninstall (){
-	rm -f "$install_dir/z"
-	rm -f "$install_dir/uz"
+	Run rm -f "$install_dir/z"
+	Run rm -f "$install_dir/uz"
 	exit
 }
 
 Help(){
-	printf "Options:\n\ti\tinstall\n\tu\tuninstall\n"
+	echo "Options:"
+	echo "    Install Dir = $install_dir"
+	echo "    Lua Shebang = $lua_shebang"
+	echo "Commands:"
+	echo "    i -> install"
+	echo "    u -> uninstall"
 	exit
 }
 
@@ -29,8 +36,8 @@ Help(){
 
 for arg in $@; do
 	case $arg in
-		'i') Install ;;
-		'u') Uninstall ;;
-		*)   Help ;;
+		'i') Install;;
+		'u') Uninstall;;
+		*)   Help;;
 	esac
 done
